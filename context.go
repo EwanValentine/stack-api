34eb9edc8de4b4cc34a585812a86652ba9bff43a
Ewan Valentine <ewan.valentine89@gmail.com>
2016-12-14 09:09:52 +0000

    UPDATED to expose request and response

--- context.go
+++ context.go
@@ -14,8 +14,8 @@ type Context struct {
 	Datastore *Datastore
 
 	// Http data
-	request  *http.Request
-	response http.ResponseWriter
+	Request  *http.Request
+	Response http.ResponseWriter
 
 	// Mux params
 	Params map[string]string
@@ -24,8 +24,8 @@ type Context struct {
 // NewContext - Return a new context.
 func NewContext(w http.ResponseWriter, r *http.Request) *Context {
 	ctx := &Context{
-		request:  r,
-		response: w,
+		Request:  r,
+		Response: w,
 	}
 	ctx.Init()
 	return ctx
@@ -33,21 +33,21 @@ func NewContext(w http.ResponseWriter, r *http.Request) *Context {
 
 // Init - Initialise http context.
 func (c *Context) Init() {
-	c.Params = mux.Vars(c.request)
+	c.Params = mux.Vars(c.Request)
 }
 
 // Bind - Bind's json data to empty struct.
 func (c *Context) Bind(payload interface{}) error {
 
 	// Decode body string into a string
-	decoder := json.NewDecoder(c.request.Body)
+	decoder := json.NewDecoder(c.Request.Body)
 	err := decoder.Decode(&payload)
 
 	if err != nil {
 		return err
 	}
 
-	defer c.request.Body.Close()
+	defer c.Request.Body.Close()
 
 	return nil
 }
@@ -55,20 +55,20 @@ func (c *Context) Bind(payload interface{}) error {
 // JSON - Set header to JSON
 func (c *Context) JSON(data interface{}, code int) {
 	c.SetHttpCode(code)
-	c.response.Header().Set("Content-Type", "application/json")
+	c.Response.Header().Set("Content-Type", "application/json")
 	json, err := json.Marshal(data)
 
 	if err != nil {
-		http.Error(c.response, err.Error(), http.StatusInternalServerError)
+		http.Error(c.Response, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	c.response.Write(json)
+	c.Response.Write(json)
 }
 
-// Set - Sets http response header code.
+// Set - Sets http Response header code.
 func (c *Context) SetHttpCode(code int) {
-	c.response.WriteHeader(code)
+	c.Response.WriteHeader(code)
 }
 
 // Param - Get param by name
@@ -78,5 +78,5 @@ func (c *Context) Param(name string) string {
 
 // Header - Get a header value
 func (c *Context) Header(name string) string {
-	return c.request.Header.Get(name)
+	return c.Request.Header.Get(name)
 }
